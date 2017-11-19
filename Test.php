a280bbce73c2d2be842e7a3899ec6930f50bcf0e
Gomasy <nyan@gomasy.jp>
2017-11-19 11:21:02 +0900

    typo

--- Test.php
+++ Test.php
@@ -4,6 +4,6 @@ use PHPUnit\Framework\TestCase;
 class Test extends TestCase
 {
     public function testExample() {
-        $this->assertsEquals(1 + 1, 2);
+        $this->assertEquals(1 + 1, 2);
     }
 }
